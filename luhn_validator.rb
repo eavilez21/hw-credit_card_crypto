ad46cb5ebc31cc7a8421a7f03c60849e1c6dedaa
Emmy Avilez <avileze@yahoo.com>
2022-03-10 21:06:04 +0800

    Luhn Validator module

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Verifying credit card check digit
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,6 +8,8 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
+    transform_number = nums_a.reverse!.map.with_index { |num, index| index.even? ? num : (num * 2).digits.sum }
+    (transform_number.reduce(:+) % 10).zero?
 
     # TODO: use the integers in nums_a to validate its last check digit
   end
